07f596eab8c4a10e0ed5938e65804a60242a749f
WeiShiyu1214 <46751066+WeiShiyu1214@users.noreply.github.com>
2019-02-09 22:56:50 +0000

    Add files via upload

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -2,17 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-     name=models.CharField(max_length=128,unique=True)
+	name = models.CharField(max_length=128,unique=True)
      
-def __str__(self):
- return self.name
+	def __str__(self):
+		return self.name
 
 
 class Page(models.Model):
-    category=models.ForeignKey(Category)
-    title=models.CharField(max_length=128)
-    url=models.URLField()
-    views=models.IntegerField(default=0)
+	category = models.ForeignKey(Category)
+	title = models.CharField(max_length=128)
+	url = models.URLField()
+	views = models.IntegerField(default=0)
     
-def __str__(self):
- return self.title
+	def __str__(self):
+		return self.title
